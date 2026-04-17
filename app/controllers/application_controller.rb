f16f281abf73f1e95f1b1d9f8b35acaca982ac7c
TVanderploeg1 <tiffany.j.v.5@gmail.com>
2026-04-17 16:09:02 -0500

    extend devise registrations to support user signup and profile updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    profile_keys = [:full_name, :address, :city, :postal_code, :province_id]
+    devise_parameter_sanitizer.permit(:sign_up, keys: profile_keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: profile_keys)
+  end
 end
