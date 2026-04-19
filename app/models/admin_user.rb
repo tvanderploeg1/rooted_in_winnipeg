a2c0b363300280176202afe3a9dd67d424324965
TVanderploeg1 <tiffany.j.v.5@gmail.com>
2026-04-19 13:20:09 -0500

    fix activeadmin admin users

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,14 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at email id remember_created_at reset_password_sent_at updated_at]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    []
+  end
 end
